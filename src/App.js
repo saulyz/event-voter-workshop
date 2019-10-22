2364389aa9ccf1e836cec81709d39aba3a1f7fb9
Arminas Katilius <akatilius@kayak.com>
2019-10-22 21:54:23 +0300

    Clean up React App and add initial layout

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Grid from '@material-ui/core/Grid';
 function App() {
   return (
     <Grid container>
-        <Grid item xs={6}>
+        <Grid item xs={6} style={{backgroundColor: 'green'}}>
             A
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={6} direction="row" justify="center" alignItems="center">
+          <Grid item xs={12} style={{backgroundColor: 'red'}}>
             B
-        </Grid>
-        <Grid item xs={12}>
+          </Grid>
+          <Grid item xs={12} style={{backgroundColor: 'red'}}>
             C
+          </Grid>
         </Grid>
+
     </Grid>
   );
 }
