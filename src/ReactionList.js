859154eebbe5ee400cddd63517ac5b2c642cbe8e
Arminas Katilius <akatilius@kayak.com>
2019-10-23 11:48:22 +0300

    Add modal for reaction form

--- src/ReactionList.js
+++ src/ReactionList.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import AddReactionForm from './AddReactionForm';
 import ScoreIcon from './ScoreIcon';
+import { Dialog } from '@material-ui/core';
 
 export default function ReactionList({ reactions, onAddReaction }) {
   const [formVisible, setFormVisible] = useState(false);
 
   function handleAddReaction(reaction) {
     onAddReaction(reaction);
+    closeForm();
+  }
+  function closeForm() {
     setFormVisible(false);
   }
   return (
@@ -19,7 +23,9 @@ export default function ReactionList({ reactions, onAddReaction }) {
         ))}
       </ul>
       <button onClick={() => setFormVisible(true)}>Add Reaction</button>
-      {formVisible && <AddReactionForm onAddReaction={handleAddReaction} />}
+      <Dialog onClose={closeForm} open={formVisible}>
+        <AddReactionForm onAddReaction={handleAddReaction} />
+      </Dialog>
     </>
   );
 }
